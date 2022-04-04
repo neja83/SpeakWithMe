eb3e5e7b518d6708dd2e6d5524908ebbe0255875
Евгений Крапивенко <ekrapivenko@gmail.com>
2022-04-04 17:48:47 +0300

    Initial Commit

---
+++ SpeakWithMe/SpeakWithMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakWithMeApp.swift
+//  SpeakWithMe
+//
+//  Created by Eugene Krapivenko on 04.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpeakWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
